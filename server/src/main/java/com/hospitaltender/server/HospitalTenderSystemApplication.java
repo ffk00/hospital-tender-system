f8dc419c6c8158f45370d16586ecc0b11fd02686
Fatih F. Keser <keserfatihfurkan@proton.me>
2026-02-01 12:28:08 +0300

    Initial commit

---
+++ server/src/main/java/com/hospitaltender/server/HospitalTenderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hospitaltender.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalTenderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalTenderSystemApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/hospitaltender/server/HospitalTenderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitaltender.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalTenderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
